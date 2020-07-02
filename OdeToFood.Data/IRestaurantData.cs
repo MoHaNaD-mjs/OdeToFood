e34b5891bc1dd17801517feba6351d5adb94389e
Mohanad Jaber <mohanadmjs9990@gmail.com>
2020-07-02 09:57:56 +0300

    Final step for trining asp fundamentals

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,19 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        //IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string Name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updateRestaurant);       
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
+        int GetCoutOfREstaurants();
+        int Commit();
+    }
+}
